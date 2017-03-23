7e7e95bf0a5059b9b10afdf84524a05d48c4fa44
eyifemo <phil.mo@ericsson.com>
2017-03-23 23:05:25 +0800

    format code

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-class xadmin{
-    constructor(host, port){
+class xadmin {
+    constructor(host, port) {
         this.host = host;
         this.port = port;
         this.app = express();
     }
 
-    init(){
+    init() {
         // view engine setup
         this.app.set('views', path.join(__dirname, 'views'));
         this.app.set('view engine', 'ejs');
@@ -32,28 +32,28 @@ class xadmin{
         this.app.use('/users', users);
 
         // catch 404 and forward to error handler
-        this.app.use(function(req, res, next) {
-        var err = new Error('Not Found');
-        err.status = 404;
-        next(err);
+        this.app.use(function (req, res, next) {
+            var err = new Error('Not Found');
+            err.status = 404;
+            next(err);
         });
 
         // error handler
-        this.app.use(function(err, req, res, next) {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
+        this.app.use(function (err, req, res, next) {
+            // set locals, only providing error in development
+            res.locals.message = err.message;
+            res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-        // render the error page
-        res.status(err.status || 500);
-        res.render('error');
+            // render the error page
+            res.status(err.status || 500);
+            res.render('error');
         });
     }
 
-    start(){
+    start() {
         this.app.listen(this.port);
     }
-    
+
 }
 
 module.exports = xadmin;
